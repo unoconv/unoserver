37f815dd19e4c677337890b8da22f18e20fd440a
Lennart Regebro <regebro@gmail.com>
2021-06-11 14:06:12 +0200

    A first server command

--- src/unoserver/server.py
+++ src/unoserver/server.py
@@ -1,2 +1,42 @@
+import argparse
+import subprocess
+import tempfile
+from urllib import request
+
+
+def server(interface="127.0.0.1", port="2002"):
+    print("Starting unoserver.")
+
+    with tempfile.TemporaryDirectory() as tmpuserdir:
+
+        connection = "socket,host=%s,port=%s,tcpNoDelay=1;urp;StarOffice.ComponentContext" % (interface, port)
+        tmp_uri = 'file://' + request.pathname2url(tmpuserdir)
+
+        cmd = [
+            "libreoffice",
+            "--headless",
+            "--invisible",
+            "--nocrashreport",
+            "--nodefault",
+            "--nologo",
+            "--nofirststartwizard",
+            "--norestore",
+            f"-env:UserInstallation={tmp_uri}",
+            f"--accept={connection}"
+        ]
+
+        print(cmd)
+        subprocess.call(cmd)
+
+
 def main():
-    print("UNOSERVER!")
+    parser = argparse.ArgumentParser("unoserver")
+    parser.add_argument("--interface", default="127.0.0.1", help="The interface used by the server")
+    parser.add_argument("--port", default="2002", help="The port used by the server")
+    args = parser.parse_args()
+
+    server(args.interface, args.port)
+
+
+if __name__ == "__main__":
+    main()
