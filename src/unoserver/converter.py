24620681c667dcec47ecc70c618de5d91f4b4709
Lennart Regebro <regebro@gmail.com>
2021-06-16 16:51:44 +0200

    Simple converting that works

--- src/unoserver/converter.py
+++ src/unoserver/converter.py
@@ -1,38 +1,122 @@
 import argparse
+import os
+import sys
 import uno
-
 
 from com.sun.star.beans import PropertyValue
 
+SFX_FILTER_IMPORT = 1
+SFX_FILTER_EXPORT = 2
+DOC_TYPES = {
+    "com.sun.star.sheet.SpreadsheetDocument",
+    "com.sun.star.text.TextDocument",
+    "com.sun.star.presentation.PresentationDocument",
+    "com.sun.star.drawing.DrawingDocument",
+    "com.sun.star.sdb.DocumentDataSource",
+    "com.sun.star.formula.FormulaProperties",
+    "com.sun.star.script.BasicIDE",
+}
 
-def converter(infile, outfile, interface="127.0.0.1", port="2002"):
-    print("Starting unoconverter.")
 
-    # Get the uno component context from the PyUNO runtime
-    localContext = uno.getComponentContext()
-    resolver = localContext.ServiceManager.createInstanceWithContext(
-        "com.sun.star.bridge.UnoUrlResolver", localContext
-    )
+def prop2dict(properties):
+    return {p.Name: p.Value for p in properties}
 
-    ctx = resolver.resolve(
-        "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext"
-    )
 
-    # Declare the ServiceManager
-    smgr = ctx.ServiceManager
-    desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx)
+class UnoConverter:
+    def __init__(self, interface="127.0.0.1", port="2002"):
+        print("Starting unoconverter.")
 
-    path = uno.systemPathToFileUrl(infile)
-    document = desktop.loadComponentFromURL(path, "_default", 0, ())
+        self.local_context = uno.getComponentContext()
+        self.resolver = self.local_context.ServiceManager.createInstanceWithContext(
+            "com.sun.star.bridge.UnoUrlResolver", self.local_context
+        )
+        self.context = self.resolver.resolve(
+            "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext"
+        )
+        self.service = self.context.ServiceManager
+        self.desktop = self.service.createInstanceWithContext(
+            "com.sun.star.frame.Desktop", self.context
+        )
+        self.filters = self.service.createInstanceWithContext(
+            "com.sun.star.document.FilterFactory", self.context
+        )
+        self.types = self.service.createInstanceWithContext(
+            "com.sun.star.document.TypeDetection", self.context
+        )
 
-    print(f"To {path}")
-    path = uno.systemPathToFileUrl(outfile)
+    def get_doc_type(self, doc):
+        for t in DOC_TYPES:
+            if doc.supportsService(t):
+                return t
+        return ""
 
-    args = (
-        PropertyValue(Name="FilterName", Value="writer_pdf_Export"),
-        PropertyValue(Name="Overwrite", Value=True),
-    )
-    document.storeToURL(path, args)
+    def find_filter(self, import_type, export_type):
+        # List export filters. You can only search on module, iflags and eflags,
+        # so the import and export types we have to test in a loop
+        export_filters = self.filters.createSubSetEnumerationByQuery(
+            "getSortedFilterList():iflags=2"
+        )
+
+        candidates = []
+        while export_filters.hasMoreElements():
+            # Filter DocumentService here
+            export_filter = prop2dict(export_filters.nextElement())
+            if export_filter["DocumentService"] != import_type:
+                continue
+            if export_filter["Type"] != export_type:
+                continue
+
+            candidates.append(export_filter)
+
+        if len(candidates) == 1:
+            return candidates[0]["Name"]
+
+        if len(candidates) == 0:
+            print(
+                "Unknown file extension {ext}, please specify type with --export-type"
+            )
+            sys.exit(1)
+
+        print("Ambigous file extension, please specify type with --export-type")
+        print("Examples:")
+        [print(e.Type) for e in candidates]
+        sys.exit(1)
+
+    def convert(self, infile, outfile, export_filter=None):
+
+        ### Prepare some things
+
+        export_path = uno.systemPathToFileUrl(outfile)
+        if not export_filter:
+            export_type = self.types.queryTypeByURL(export_path)
+            if not export_type:
+                print(
+                    "Unknown export file type, please specify type with --export-type"
+                )
+
+        # TODO: Verify that infile exists and is openable, and that outdir exists, because uno's
+        # exceptions are completely useless!
+
+        ### Load the document
+        import_path = uno.systemPathToFileUrl(infile)
+        # This returned None if the file was locked, I'm hoping the ReadOnly flag avoids that.
+        print(f"Opening {infile}")
+        document = self.desktop.loadComponentFromURL(
+            import_path, "_default", 0, (PropertyValue(Name="ReadOnly", Value=True),)
+        )
+
+        # Figure out document type:
+        import_type = self.get_doc_type(document)
+        filtername = self.find_filter(import_type, export_type)
+
+        print(f"Exporting to {outfile}")
+        print(f"Using {filtername} export filter")
+
+        args = (
+            PropertyValue(Name="FilterName", Value=filtername),
+            PropertyValue(Name="Overwrite", Value=True),
+        )
+        document.storeToURL(export_path, args)
 
 
 def main():
@@ -49,7 +133,8 @@ def main():
     )
     args = parser.parse_args()
 
-    converter(args.infile, args.outfile, args.interface, args.port)
+    converter = UnoConverter(args.interface, args.port)
+    converter.convert(args.infile, args.outfile)
 
 
 if __name__ == "__main__":
