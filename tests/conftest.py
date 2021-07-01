f9f4646c37a4faf668ec7227aa1e5b75e4b3a38c
Lennart Regebro <regebro@gmail.com>
2021-07-01 14:44:55 +0200

    Increase the wait time for LibreOffice to start, it fails too often on Github

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ def server_fixture():
     srvr = server.UnoServer()
     process = srvr.start(daemon=True)
     # Give libreoffice a chance to start
-    time.sleep(5)
+    time.sleep(8)
     yield process  # provide the fixture value
     print("Teardown Unoserver")
     process.terminate()
