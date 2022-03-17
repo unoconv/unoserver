0d6cc47e5c87685a8428952e0989bc21d6b04460
Lennart Regebro <regebro@gmail.com>
2022-03-17 12:55:44 +0100

    Fix a test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from unoserver import server
 @pytest.fixture(scope="session")
 def server_fixture():
     srvr = server.UnoServer()
-    process = srvr.start(daemon=True)
+    process = srvr.start()
     # Give libreoffice a chance to start
     time.sleep(8)
     yield process  # provide the fixture value
